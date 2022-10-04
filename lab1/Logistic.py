3d3dd7de4686bb31e21ca011fb7863a4ee47c275
wintermelon008 <1904346407@qq.com>
2022-10-04 20:07:54 +0800

    finish logistic.py

--- lab1/Logistic.py
+++ lab1/Logistic.py
@@ -7,15 +7,46 @@ class LogisticRegression:
 
     def sigmoid(self, x):
         """The logistic sigmoid function"""
+        return 1.0 / (1 + np.exp(-x))
 
-    def fit(self, X, y, lr=0.01, tol=1e-7, max_iter=1e7):
+    def fit(self, X: np.ndarray, y: np.ndarray , lr=0.001, tol=1e-7, max_iter=1e7):
         """
         Fit the regression coefficients via gradient descent or other methods 
         """
+        m, n = X.shape      # X is m rows n colums
+
+        self.w = np.random.uniform(low=0.0, high=1.0, size=n).reshape(-1, 1)
+        loss_list = []
+
+        for i in range(int(max_iter)):
+            temp = np.dot(X, self.w)
+            loss = np.dot(-y.T, temp) + np.log(1 + np.exp(temp)).sum()
+            loss_list.append(loss)
+            self.w -= (lr * np.dot(X.T, self.sigmoid(temp) - y))
+            if i > 2 and abs(loss_list[i] - loss_list[i - 1]) < tol:
+                break
+        
+        return loss_list
+
         
     def predict(self, X):
         """
         Use the trained model to generate prediction probabilities on a new
         collection of data points.
         """
+
+        m, n = X.shape
+        result = []
+
+        for i in range(m):
+            x_i = X[i].reshape(-1, 1)
+            output_i = np.dot(self.w.T, x_i)
+            p1_i =  self.sigmoid(output_i)
+            if p1_i > 0.5:
+                result.append(1)
+            else:
+                result.append(0)
+
+        
+        return result
         
