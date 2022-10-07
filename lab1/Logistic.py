0354be4a439abbd17124f21e276b884777922c9f
wintermelon008 <1904346407@qq.com>
2022-10-07 16:07:17 +0800

    finish lab1

--- lab1/Logistic.py
+++ lab1/Logistic.py
@@ -9,10 +9,39 @@ class LogisticRegression:
         """The logistic sigmoid function"""
         return 1.0 / (1 + np.exp(-x))
 
-    def fit(self, X: np.ndarray, y: np.ndarray , lr=0.001, tol=1e-7, max_iter=1e7):
+    def fit(self, X: np.ndarray, y: np.ndarray , lr=0.001, tol=1e-7, max_iter=1e7, method='gradient'):
         """
         Fit the regression coefficients via gradient descent or other methods 
         """
+        assert method in ['gradient', 'newton'], "method must be 'gradient' or 'newton', but got {}".format(method)
+
+        if method == 'gradient':
+            return self.gradient(X, y, lr, tol, max_iter)
+        elif method == 'newton':
+            return self.newton(X, y, lr, tol, max_iter)
+
+
+
+    def gradient(self, X: np.ndarray, y: np.ndarray , lr=0.001, tol=1e-7, max_iter=1e7):
+        
+        m, n = X.shape    
+        self.w = np.random.uniform(low=0.0, high=1.0, size=n).reshape(-1, 1)
+        loss_list = []
+
+        for i in range(int(max_iter)):
+            temp = np.dot(X, self.w)
+            loss = np.dot(-y.T, temp) + np.log(1 + np.exp(temp)).sum()
+            loss_list.append(loss[0][0])
+            delta1 = (np.dot(X.T, self.sigmoid(temp) - y))
+            if i > 2 and np.dot(delta1.T, delta1) < tol:
+                break
+            self.w -= lr * delta1
+        
+        return i, loss_list
+
+    
+
+    def newton(self, X: np.ndarray, y: np.ndarray , lr=0.001, tol=1e-7, max_iter=1e7):
         m, n = X.shape    
 
         self.w = np.random.uniform(low=0.0, high=1.0, size=n).reshape(-1, 1)
@@ -22,11 +51,17 @@ class LogisticRegression:
             temp = np.dot(X, self.w)
             loss = np.dot(-y.T, temp) + np.log(1 + np.exp(temp)).sum()
             loss_list.append(loss[0][0])
-            self.w -= (lr * np.dot(X.T, self.sigmoid(temp) - y))
-            if i > 2 and abs(loss_list[i] - loss_list[i - 1]) < tol:
+            diag_p1 = np.diag(self.sigmoid(temp).reshape(-1)) 
+            diag = np.dot(diag_p1, np.identity(m) - diag_p1)
+            delta1 = (np.dot(X.T, self.sigmoid(temp) - y))
+            
+            if i > 2 and np.dot(delta1.T, delta1) < tol:
                 break
+            delta2 = np.dot(np.dot(X.T, diag), X)
+            self.w -= np.dot(np.linalg.inv(delta2), delta1)
         
         return i, loss_list
+        
 
         
     def predict(self, X):
