55aa4490c9ea834cd4f345933cb38b3fe94a0a18
wintermelon008 <1904346407@qq.com>
2022-12-24 21:12:37 +0800

    [Lab5] update SL

--- lab5/src/Methods/LinearReg.py
+++ lab5/src/Methods/LinearReg.py
@@ -1,20 +1,34 @@
 # 线性回归模型
 import numpy as np
-
+import pickle
+import time
 
 
 class LinearRegression:
 
-    def __init__(self):
-        pass
-
-
-    def fit(self, X: np.ndarray, y: np.ndarray):
+    def __init__(self, X: np.ndarray, y: np.ndarray):
         self.X = X
         self.y = y
+        self._X = np.linalg.inv(self.X.T @ self.X) @ self.X.T @ self.y
+        
+
+    def fit(self):
+        pass
         
     def predict(self, X:np.ndarray) -> np.ndarray: 
-        y_pre = X @ (np.linalg.inv(self.X.T @ self.X) @ self.X.T @ self.y)
+        y_pre = X @ self._X
         y_pre = y_pre.reshape(-1, 1)
        
         return y_pre
+    
+    
+    def save(self, filename = None):
+        
+        if filename == None:
+            t_str = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
+            filename = "./model/Model_LinearReg" + "_" + t_str + ".dat"
+            
+        pickle.dump(self, open(filename, "wb"))
+
+
+                
--- lab5/src/Methods/XGBoost.py
+++ lab5/src/Methods/XGBoost.py
@@ -1,8 +1,54 @@
 # XGBoost
+import numpy as np
+from . import RegTree as M_REG
 
 
-class XGBoost:
+class XGBoost(object):
+    
+    def __init__(self, X_train:np.ndarray, y_train:np.ndarray) -> None:
+        self.X = X_train
+        self.y = y_train
+        self.m, self.n = self.X.shape
+        self.treeset = []
+        self.train_err = []
+
+    def fit(self, T = 10, min_train_err = 1e-6, tree_parameters: dict = {}):
+        m, n = self.y.shape
+        self.T = T
+        y_t = np.zeros((m, 1))
+        
+        boost_if_silent = 1
+        if tree_parameters.__contains__("if_silent"):
+            boost_if_silent = tree_parameters["if_silent"]
+        
+        for i in range(0, self.T):
+            tree = M_REG.RegressionTree(self.X, self.y, y_t)
+            tree.fit(tree_parameters)
+            
+            y_t = y_t + tree.predict(self.X)
+            self.treeset.append(tree)
+            err = tree._get_err(y_t)
+            self.train_err.append(err)
+            if err < min_train_err:
+                break
+        
+            if boost_if_silent == 0:
+                print("Tree No.{} is done.".format(i))
     
     
-    def __init__(self) -> None:
-        pass
+    def predict(self, X_test:np.ndarray):
+        m, n = X_test.shape
+        pre = np.zeros((m, 1))
+        for tree in self.treeset:
+            pre = pre + tree.predict(X_test)
+        return pre
+    
+    
+    def RMSE(self, pre:np.ndarray, val:np.ndarray):
+        y = val - pre
+        m, _ = y.shape
+        return np.sqrt((y.T @ y) / m)[0][0]
+    
+    def R2(self, pre:np.ndarray, val:np.ndarray):
+        var = np.var(val)
+        return(1 - (self.RMSE(pre, val) ** 2)/ var)
